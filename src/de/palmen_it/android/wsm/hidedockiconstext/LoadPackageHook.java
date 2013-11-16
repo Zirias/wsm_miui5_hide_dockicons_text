fddbe1dabcbf7ab7803f20733a8607262c3de168
Felix Palmen <felix@palmen-it.de>
2013-11-16 10:23:22 +0100

    code formatting

--- src/de/palmen_it/android/wsm/hidedockiconstext/LoadPackageHook.java
+++ src/de/palmen_it/android/wsm/hidedockiconstext/LoadPackageHook.java
@@ -13,38 +13,48 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class LoadPackageHook implements IXposedHookLoadPackage {
 
-    private static void log(String log) {
-        XposedBridge.log("[HideDockIconsText] " + log);
-    }
+	private static void log(String log) {
+		XposedBridge.log("[HideDockIconsText] " + log);
+	}
 
 	@Override
 	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
-        if (lpparam.packageName.equals("com.miui.home")) {
-            log("Start hook in: " + lpparam.packageName);
-            ClassLoader classLoader = lpparam.classLoader;
-            try {
-                    Class.forName("com.miui.home.launcher.ShortcutIcon", false, classLoader);
-                    log("Searching for methods...");
-                    XposedHelpers.findAndHookMethod("com.miui.home.launcher.ShortcutIcon", classLoader,
-                                    "onFinishInflate", new XC_MethodHook() {
-                                            @Override
-                                            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                                                    FrameLayout v = (FrameLayout) param.thisObject;
-                                                    Method lp = v.getClass().getMethod("getLayoutParams");
-                                                    if (lp == null) return;
-                                                    if (lp.invoke(v).toString().startsWith("android.view.ViewGroup$LayoutParams"))
-                                                    {
-                                                    	final Context context = v.getContext();
-                                                    	int resID = context.getResources().getIdentifier("icon_title", "id", "com.miui.home");
-                                                    	v.findViewById(resID).setVisibility(View.INVISIBLE);
-                                                    }
-                                            }
-                    });
-            } catch (ClassNotFoundException ignored) {
-                    log("Class not found! Skipping...");
-            } catch (NoSuchMethodError ignored) {
-                    log("Method not found! Skipping...");
-            }
-        }
-    }
+		if (lpparam.packageName.equals("com.miui.home")) {
+			log("Start hook in: " + lpparam.packageName);
+			ClassLoader classLoader = lpparam.classLoader;
+			try {
+				Class.forName("com.miui.home.launcher.ShortcutIcon", false,
+						classLoader);
+				log("Searching for methods...");
+				XposedHelpers.findAndHookMethod(
+						"com.miui.home.launcher.ShortcutIcon", classLoader,
+						"onFinishInflate", new XC_MethodHook() {
+							@Override
+							protected void afterHookedMethod(
+									MethodHookParam param) throws Throwable {
+								FrameLayout v = (FrameLayout) param.thisObject;
+								Method lp = v.getClass().getMethod(
+										"getLayoutParams");
+								if (lp == null)
+									return;
+								if (lp.invoke(v)
+										.toString()
+										.startsWith(
+												"android.view.ViewGroup$LayoutParams")) {
+									final Context context = v.getContext();
+									int resID = context.getResources()
+											.getIdentifier("icon_title", "id",
+													"com.miui.home");
+									v.findViewById(resID).setVisibility(
+											View.INVISIBLE);
+								}
+							}
+						});
+			} catch (ClassNotFoundException ignored) {
+				log("Class not found! Skipping...");
+			} catch (NoSuchMethodError ignored) {
+				log("Method not found! Skipping...");
+			}
+		}
+	}
 }
